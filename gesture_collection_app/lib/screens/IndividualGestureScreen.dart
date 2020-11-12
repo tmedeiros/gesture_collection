94cbf92ef70f34a7f16c5ffeaa9888b7650d386d
Jagdish Katariya <katariyajagdish05@gmail.com>
2020-11-12 03:44:20 -0600

    added two Gesture Library Pages with fake dynamic data

--- gesture_collection_app/lib/screens/IndividualGestureScreen.dart
+++ gesture_collection_app/lib/screens/IndividualGestureScreen.dart
@@ -1,23 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:gesture_collection_app/widgets/top_nav_bar.dart';
 
+import 'LibraryScreen.dart';
+
 class IndividualGestureScreen extends StatelessWidget {
   static const String routeName = "/individualgesture/{}";
 
   @override
   Widget build(BuildContext context) {
+    Todo todo = ModalRoute.of(context).settings.arguments;
+    // Text title=Text(todo.title);
+    // Text description1=todo.description;
+
     return Scaffold(
       appBar: TopNavBarWidget(
-        height: 300,
-        title: "Gesture 1",
+        height: 60,
+        title: todo.title,
         backlink: "/library",
         navEnabled: true,
 
       ),
-      body: Container(
-          child: Center(
-            child: Text("Individual Gesture Screen"),
-          )),
-    );
+      body:
+      SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Card(
+          child: Column(
+            children: [
+              Expanded(
+                child: Image.asset('assets/images/gestures.jpeg'),
+              ),
+              Divider(),
+              Text(
+                todo.description,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+
+        // ListTile(
+              //   title: Text('(408) 555-1212',
+              //       style: TextStyle(fontWeight: FontWeight.w500)),
+              //   leading: Icon(
+              //     Icons.contact_phone,
+              //     color: Colors.blue[500],
+              //   ),
+              // ),
+            ],
+          ),
+        ),
+      )
+      //   Center(
+    //     child:Stack(
+    //         alignment: Alignment.topCenter,
+    //         children: <Widget>[
+    //         Container(
+    //         height: 300.0,
+    //         decoration: BoxDecoration(
+    //         image: DecorationImage(
+    //         fit: BoxFit.cover,
+    //         alignment: FractionalOffset.center,
+    //         image: AssetImage('assets/images/gestures.jpeg'),
+    //   )),
+    // ),
+    //   Container(
+    //   margin: EdgeInsets.only(top: 100),
+    //   width: MediaQuery.of(context).size.width * 0.92,
+    //   child: Padding(
+    //   padding: const EdgeInsets.all(8.0),
+    //   child: Text("jitmmnmnmesh",
+    //   style: TextStyle(fontSize: 19),
+    //   maxLines: 1,
+    //   textAlign: TextAlign.center)),
+    //   decoration: BoxDecoration(
+    //   color: Colors.white,
+    //   boxShadow: [
+    //   BoxShadow(
+    //   color: Colors.grey.withOpacity(0.8),
+    //   spreadRadius: 1,
+    //   blurRadius: 3,
+    //   offset: Offset(1, 1),
+    //   ),
+    //   ],
+    //   ),
+    //   )
+    //   ],
+    //   ),
+    //     )
+        // Container(
+        //     child: Center(
+        //       child: Text(todo.title),
+        //       image: Image.network('https://picsum.photos/250?image=9'),
+        //     )),
+      );
   }
 }
--- gesture_collection_app/lib/screens/LibraryScreen.dart
+++ gesture_collection_app/lib/screens/LibraryScreen.dart
@@ -1,27 +1,131 @@
 import 'package:flutter/material.dart';
+import 'package:gesture_collection_app/screens/IndividualGestureScreen.dart';
 import 'package:gesture_collection_app/widgets/top_nav_bar.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'dart:developer';
+// import 'package:firebase_auth/firebase_auth.dart';
 
+// import 'package:firebase_test/home.dart';
+// void main() {
+//   runApp(LibraryScreen(
+//     items: List<String>.generate(10000, (i) => "Item $i"),
+//   ));
+// }
+class Todo {
+  final String title;
+  final String description;
+  Todo(this.title, this.description);
+
+}
+
+//Firebase initialization setup
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(LibraryScreen());
+}
 class LibraryScreen extends StatelessWidget {
+  final fb = FirebaseDatabase.instance;
+
+  Todo todo;
   static const String routeName = "/library";
+  // final List<String> items;
+  // LibraryScreen({Key key, @required this.items}) : super(key: key);
+  final items = List<String>.generate(10000, (i) => "Item $i");
+  // void onTapped(Post post) {
+  //   // navigate to the next screen.
+  // }
+  var retrievedTitle;
+
+  final todos = List<Todo>.generate(
+    20,
+        (i) => Todo(
+      'Gesture $i',
+      'Gesture ======>>>> $i'
+          '\nmovement or position of the hand, arm, body, head, or'
+          ' face that is expressive of an idea, opinion, emotion, etc.: '
+          'the gestures of an orator; a threatening gesture. the use of '
+          'such movements to express thought, emotion, etc. '
+          'movement or position of the hand, arm, body, head, or'
+          ' face that is expressive of an idea, opinion, emotion, etc.: '
+          'the gestures of an orator; a threatening gesture. the use of '
+          'such movements to express thought, emotion, etc. ',
+    ),
+  );
+  // List<Post> posts = ["edasd","sdadasd","asdasdad"]; // your list of contents.
 
   @override
   Widget build(BuildContext context) {
+    final ref = fb.reference();
+    Todo todo;
+    ref.child("gesturedata-76273").once().then((DataSnapshot data){
+      print(data.value);
+      print(data.key);
+      // setState(() {
+        retrievedTitle = data.value;
+        log(data.value);
+        // todos.add(data.value);
+      // });
+    });
     return Scaffold(
       appBar: TopNavBarWidget(
-        height: 300,
+        height: 60,
         title: "Gesture Library",
         backlink: "/speech",
         navEnabled: true,
       ),
-      body: Container(
-          child: Center(
-            child: TextButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed("/individualgesture/1");
-                }),
-            //TODO: Implement list of gestures using labels.dart
-            //TODO: Create individual gesture widget
-          )),
+        body: ListView.separated(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Icon(Icons.wb_sunny),
+              title: Text(todos[index].title),
+              trailing: Icon(Icons.keyboard_arrow_right),
+
+              onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => IndividualGestureScreen(),
+                      settings: RouteSettings(
+                        arguments: todos[index],
+                      ),
+                    ),
+                  );
+                },
+            );
+          },
+          separatorBuilder: (context, index) {
+            return Divider();
+          },
+        ),
+
+
+      // body: ListView.builder(
+      //   itemCount: items.length,
+      //   itemBuilder: (context, index) {
+      //     return ListTile(
+      //       title: Text('${items[index]}'),
+      //     );
+      //   },
+      // ),
+      // body: Container(
+      //
+      //     child: Center(
+      //         child: ElevatedButton(
+      //             child: Text('Open route'),
+      //             onPressed: () {
+      //
+      //             // Navigator.pushNamed(context,"/individualgesture/1");
+      //             Navigator.push(
+      //               context,
+      //               MaterialPageRoute(builder: (context) => IndividualGestureScreen()),
+      //             );
+      //           }),
+      //       //TODO: Implement list of gestures using labels.dart
+      //       //TODO: Create individual gesture widget
+      //     )),
     );
   }
 }
